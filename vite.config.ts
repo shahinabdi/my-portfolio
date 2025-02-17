91655a814cc2beafefdbfb7c6ec0dd1f2843b24e
Shahin ABDI <shahin.abdi.pro@gmail.com>
2025-02-18 00:08:25 +0100

    ghActions added

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
   },
-});
+})
